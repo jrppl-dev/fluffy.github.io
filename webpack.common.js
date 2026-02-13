108d56bcf07724c66dd8b664bfbce8188ddc4ba6
Joao Prizal <joao.prizal@inventsys.com>
2026-02-13 22:33:08 +0000

    Configure Webpack for CSS bundling and remove unused local assets

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './js/app.js',
+    style: './css/style.css',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    filename: './js/app.js',
+    filename: 'js/[name].[contenthash].js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
 };
